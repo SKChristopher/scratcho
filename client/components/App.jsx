080060ceb7439e46c4d05d4972cb480b35626a5a
Christopher <chris132128@gmail.com>
2018-05-21 22:17:10 -0700

    removed console.logs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -32,12 +32,14 @@ class App extends Component {
           class="background-image"
           src="https://i.pinimg.com/originals/38/e9/9f/38e99f0083d07f6ac1df75b6bbb0fbf2.jpg"
         />
-        <div className="white-text" >Counter: {this.state.counter}</div>
+        <div className="white-text">Counter: {this.state.counter}</div>
         <br />
         <button className="fancy-button" onClick={this.handleButtonClick}>
           Counter++
         </button>
-        <marquee scrollamount="10" direction="right" ><img src="https://media.giphy.com/media/yqMtk0TfjRa5W/giphy.gif" /></marquee>
+        <marquee scrollamount="10" direction="right">
+          <img src="https://media.giphy.com/media/yqMtk0TfjRa5W/giphy.gif" />
+        </marquee>
       </div>
     );
   }
--- server/controllers/counter.js
+++ server/controllers/counter.js
@@ -4,7 +4,7 @@ const counterController = {
   get: (req, res) => {
     // console.log("Getting counter...");
     Counter.find({}).exec((err, counter) => {
-    //   console.log(`Counter: ${counter}`);
+      // console.log(`Counter: ${counter}`);
       if (counter) res.json(counter[0].count);
       else {
         Counter.create({
@@ -15,23 +15,14 @@ const counterController = {
   },
 
   add: (req, res) => {
-    // updatedCounter = 0;
     // console.log("attempting to add to counter");
-    // Counter.find({}).exec((err, counter) => {
-    //   updatedCount = counter[0].count + 1;
-    //   console.log(`Updated count: ${updatedCount}`);
-    //   Counter.update(counter[0], {$inc: {"counter[0].data": counter[0].data}})
-    //   res.json(counter[0].data);
-    // });
     // Counter.update({ _id: '5b0239c4deff9389be518772'}, {$inc: {count : 1}});
     Counter.find({}, (err, counter) => {
-        if (err) return err;
-        counter[0].count += 1;
-        counter[0].save((error) => {
-          if (error) return (error);
-        });
-        res.status(200).send(true);
-      });
+      if (err) return err;
+      counter[0].count += 1;
+      counter[0].save();
+      res.send(true);
+    });
   }
 };
 

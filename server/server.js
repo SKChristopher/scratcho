dcd74b28f157120b928049e3e9ee36837e070430
Christopher <chris132128@gmail.com>
2018-05-26 09:00:02 -0700

    server paths updated

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const counterController = require('./../server/controllers/counter.js');
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', 'build/index.html')));
+
 app.get('/getCounter', counterController.get);
 app.put('/add', counterController.add);
 
